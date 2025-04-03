e4eaee4e7c48da7b3858e59d7fbb2836dad5a9e1
jtrickey <102543051+WAaronD@users.noreply.github.com>
2025-04-03 11:01:47 -0700

    Add more firmware spec options in firmware_factory.h

--- listener_program/src/firmware/firmware_factory.h
+++ listener_program/src/firmware/firmware_factory.h
@@ -2,19 +2,29 @@
 
 #include "firmware_1240.h"
 #include "firmware_1240_imu.h"
+#include "firmware_1240_fs2x.h"
+#include "firmware_1240_fs2x_imu.h"
 
 class FirmwareFactory
 {
    public:
-    static std::unique_ptr<const Firmware1240> create(bool useImu)
+    static std::unique_ptr<const Firmware1240> create(const std::string& useImu)
     {
-        if (useImu)
+        if (useImu == "FW1240")
+        {
+            return std::make_unique<const Firmware1240>();
+        }
+        else if (useImu == "FW1240-IMU")
         {
             return std::make_unique<const Firmware1240IMU>();
         }
-        else
+        else if (useImu == "FW1240-Fs2x")
         {
-            return std::make_unique<const Firmware1240>();
+            return std::make_unique<const Firmware1240Fs2x>();
+        }
+        else if (useImu = "FW1240-Fs2x-IMU")
+        {
+            return std::make_unique<const Firmware1240Fs2xIMU>();
         }
     }
 };
