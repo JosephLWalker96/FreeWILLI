1e97af9c02b7e2f3f50da2ae4e872340b1d54cb8
jtrickey <102543051+WAaronD@users.noreply.github.com>
2025-05-07 16:17:57 -0700

    Remove parameter --fs

--- simulator_program/datalogger_simulator.py
+++ simulator_program/datalogger_simulator.py
@@ -24,7 +24,7 @@ class ArgumentParserService:
         parser.add_argument('--port', default=1045, type=int, help='UDP port to send data to')
         parser.add_argument('--ip', default="192.168.7.2", type=str, help='IP address to send data to')
         parser.add_argument('--data_dir', default="simulator_data/track132_5minchunks/", type=str, help='Directory containing .npy data files')
-        parser.add_argument('--fw', default=1240, type=int, help='Firmware version to simulate')
+        parser.add_argument('--fw', default='1240', type=str, help='Firmware version to simulate')
         parser.add_argument('--loop', action='store_true', help='Enable looping over the data')
         parser.add_argument('--stretch', action='store_true', help='Normalize data values to the min/max range of 16-bit unsigned int')
         parser.add_argument('--high_act', action='store_true', help='Enable high activity mode')
@@ -33,7 +33,6 @@ class ArgumentParserService:
         parser.add_argument('--data_glitch', default=0, type=int, help='Simulate data glitch at specific data chunk index')
         parser.add_argument('--tdoa_sim', nargs='?', const=0, type=int, default=False, action=TDOASimAction, help='Channel offset amount')
         parser.add_argument('--imu', action='store_true', help='Read in IMU data from file')
-        parser.add_argument('--fs', default=10000, type=int, help='Data sampling rate')
         parsedArgs = parser.parse_args()
         return parsedArgs
 
@@ -88,22 +87,14 @@ class DataSimulator:
             self.arguments.ip = "127.0.0.1"
 
         # Firmware-specific imports (firmware- and samplerate-dependent)
-        if self.arguments.fs == 100000:
-            if self.arguments.fw == 1550:
-                import firmware_config.firmware_1550 as fwConfig
-            elif self.arguments.fw == 1240:
-                import firmware_config.firmware_1240 as fwConfig
-            else:
-                print('ERROR: Unknown firmware version')
-                sys.exit()
-        elif self.arguments.fs == 200000:
-            if self.arguments.fw == 1240:
-                import firmware_config.firmware_1240_Fs2x as fwConfig
-            else:
-                print('ERROR: Unknown firmware version')
-                sys.exit()
+        if self.arguments.fw == '1550':
+            import firmware_config.firmware_1550 as fwConfig
+        elif self.arguments.fw == '1240':
+            import firmware_config.firmware_1240 as fwConfig
+        elif self.arguments.fw == '1240_Fs2x':
+            import firmware_config.firmware_1240_Fs2x as fwConfig
         else:
-            print('ERROR: Invalid firmware-sampling rate combination')
+            print('ERROR: Unknown firmware version')
             sys.exit()
 
         # Store firmware constants for use throughout
